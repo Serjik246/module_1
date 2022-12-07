42d477e6167731c66beb04aa04bd12a279d71467
Sergey Khalaev <sergey.khalaev@gmail.com>
2022-12-08 01:00:20 +0500

    Первая версия

---
+++ lesson_2/script.js
@@ -0,0 +1,10 @@
+'use strict'
+const nameProduct = 'Cow';
+const countProduct = 33;
+const categorytProduct = 'Farming';
+const priceProduct = 110000;
+console.log('nameProduct: ', nameProduct);
+console.log('countProduct: ', countProduct);
+console.log('categorytProduct: ', categorytProduct);
+console.log('priceProduct: ', priceProduct);
+console.log('summProduct: ', countProduct * priceProduct )
