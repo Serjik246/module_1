844950c9e384e94c1a5773961744b2ae968a6020
Sergey Khalaev <sergey.khalaev@gmail.com>
2022-12-08 01:16:54 +0500

    Добавлен второй товар

--- lesson_2/script.js
+++ lesson_2/script.js
@@ -1,10 +1,23 @@
 'use strict'
-const nameProduct = 'Cow';
-const countProduct = 33;
-const categorytProduct = 'Farming';
-const priceProduct = 110000;
-console.log('nameProduct: ', nameProduct);
-console.log('countProduct: ', countProduct);
-console.log('categorytProduct: ', categorytProduct);
-console.log('priceProduct: ', priceProduct);
-console.log('summProduct: ', countProduct * priceProduct )
+{
+    const nameProduct = 'Cow';
+    const countProduct = 33;
+    const categorytProduct = 'Farming';
+    const priceProduct = 110000;
+    console.log('nameProduct: ', nameProduct);
+    console.log('countProduct: ', countProduct);
+    console.log('categorytProduct: ', categorytProduct);
+    console.log('priceProduct: ', priceProduct);
+    console.log('summProduct: ', countProduct * priceProduct)
+}
+{
+    const nameProduct = 'Orange';
+    const countProduct = 101;
+    const categorytProduct = 'Fruit';
+    const priceProduct = 12;
+    console.log(`nameProduct: ${nameProduct}`);
+    console.log(`countProduct: ${countProduct}`);
+    console.log(`categorytProduct: ${categorytProduct}`);
+    console.log(`priceProduct: ${priceProduct}`);
+    console.log(`summProduct: ${countProduct * priceProduct}`)
+}
