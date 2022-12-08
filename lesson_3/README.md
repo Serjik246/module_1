ffe6abe8a7c9c67d9357ee4c901433c3ecf4c529
Sergey Khalaev <sergey.khalaev@gmail.com>
2022-12-09 00:39:59 +0500

    lesson 3

--- lesson_3/README.md
+++ lesson_3/README.md
@@ -1,1 +1,1 @@
-#git-config
+#lesson 3
--- lesson_3/script.js
+++ lesson_3/script.js
@@ -1,5 +1,5 @@
 'use strict'
-{
+/*{
     const nameProduct = 'Cow';
     const countProduct = 33;
     const categorytProduct = 'Farming';
@@ -20,4 +20,24 @@
     console.log(`categorytProduct: ${categorytProduct}`);
     console.log(`priceProduct: ${priceProduct}`);
     console.log(`summProduct: ${countProduct * priceProduct}`)
+}*/
+{
+    const nameProduct = prompt('Наименование товара:', '');
+    console.log(`type: ${typeof nameProduct}`);
+    console.log(`nameProduct: ${nameProduct}`);
+    
+    const countProduct = +prompt('Количество товара:', +'');
+    console.log(`type: ${typeof countProduct}`);
+    console.log(`countProduct: ${countProduct}`);
+    
+    const categorytProduct = prompt('Категория товара:', '');
+    console.log(`type: ${typeof categorytProduct}`);
+    console.log(`categorytProduct: ${categorytProduct}`);
+    
+    const priceProduct = +prompt('Цена товара', +'');
+    console.log(`type: ${typeof priceProduct}`);
+    console.log(`priceProduct: ${priceProduct}`);
+    
+    alert(`На складе ${countProduct} единиц(ы) товара "${nameProduct}" категории "${categorytProduct}" на сумму ${countProduct * priceProduct}`)
+    console.log(`На складе ${countProduct} единиц(ы) товара "${nameProduct}" категории "${categorytProduct}" на сумму ${countProduct * priceProduct}`)
 }
