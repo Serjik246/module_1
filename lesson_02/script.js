2730cfc3fae82ad87227457edb3a23e55a2dba0e
Сергей <sergey.khalaev@gmail.com>
2024-11-17 16:09:07 +0500

    Первый коммит

---
+++ lesson_02/script.js
@@ -0,0 +1,10 @@
+const nameProduct = 'Стиральная машина';
+const countProduct = 3;
+const categoryProduct = 'Бытовая техника';
+const costProduct = 2000;
+
+// Задание №1
+console.log(nameProduct);
+
+// Задание №2
+console.log(costProduct*countProduct);
