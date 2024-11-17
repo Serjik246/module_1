e670768ffca1c6af06d526161a2d81af4fdf2b5e
Сергей <sergey.khalaev@gmail.com>
2024-11-17 16:48:01 +0500

    Удалил товары в script.js

--- lesson_02/script.js
+++ lesson_02/script.js
@@ -1,21 +1,2 @@
 'use strict'
 
-{
-  const item = 'smart проектор philips';
-  const count = 15;
-  const category = 'видео техника';
-  const price = 1200;
-
-  console.log(item);
-  console.log(`общая сумма товара: ${count * price}`);
-}
-
-{
-    const item = 'Мягкая игрушка "Миньон"';
-    const count = 12;
-    const category = 'Детские товары';
-    const price = 100;
-  
-    console.log(item);
-    console.log(`общая сумма товара: ${count * price}`);
-}
