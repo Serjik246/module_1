e941ac4e0f304f9cfe4176267024d9fc21afc5e2
Сергей <sergey.khalaev@gmail.com>
2024-11-21 18:28:07 +0500

    Исправил второе задание

--- lesson_10/task_02.js
+++ lesson_10/task_02.js
@@ -3,8 +3,10 @@
 import { listIPv4 } from './ipv4.js';
 
 {
-    const listIPv4Set = new Set(listIPv4);
-
-    console.log(listIPv4Set.size);
+    const getCountUniqueValues = (arr) => {
+        const arrSet = new Set(arr);
+        return arrSet.size;
+    };
+    console.log(getCountUniqueValues(listIPv4));
 }
     
